2b12ab448314a7f05b38eb8d6f951e26aaec55db
Frazer Smith <frazer.dev@outlook.com>
2023-01-26 10:58:38 +0000

    fix(index): add to permissions-policy header if one already exists (#144)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ const fp = require("fastify-plugin");
 /**
  * @author Frazer Smith
  * @description Simple plugin that adds an `onRequest` hook to opt out of Google's FLoC
- * advertising-surveillance network by setting the relevant response headers.
+ * advertising-surveillance network by setting/adding the "interest-cohort=()" directive
+ * to the Permissions-Policy response header.
  * @param {object} server - Fastify instance.
  */
 async function plugin(server) {
 	server.addHook("onRequest", async (req, res) => {
-		res.header("Permissions-Policy", "interest-cohort=()");
+		const header = res.getHeader("Permissions-Policy");
+
+		// Header can be returned as array: https://nodejs.org/docs/latest/api/http.html#requestsetheadername-value
+		if (Array.isArray(header)) {
+			header.push("interest-cohort=()");
+			res.header("Permissions-Policy", header);
+			return;
+		}
+
+		if (!header) {
+			res.header("Permissions-Policy", "interest-cohort=()");
+		} else {
+			res.header("Permissions-Policy", `${header}, interest-cohort=()`);
+		}
 	});
 }
 
