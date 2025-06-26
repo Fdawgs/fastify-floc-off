6a2ff6d10a21a7a71decedd7165d688b3329992d
Frazer Smith <frazer.dev@icloud.com>
2025-06-26 14:00:28 +0100

    refactor(index): name hook function (#276)
    easier debugging than an anon arrow function

--- src/index.js
+++ src/index.js
@@ -10,25 +10,31 @@ const fp = require("fastify-plugin");
  * @param {import("fastify").FastifyInstance} server - Fastify instance.
  */
 async function fastifyFlocOff(server) {
-	server.addHook("onRequest", async (_req, res) => {
-		const header = res.getHeader("Permissions-Policy");
+	server.addHook(
+		"onRequest",
+		async function setFlocPermissionsHeader(_req, res) {
+			const header = res.getHeader("Permissions-Policy");
 
-		/**
-		 * Headers can be returned as an array.
-		 * @see {@link https://nodejs.org/docs/latest/api/http.html#requestsetheadername-value | Node.js HTTP API}
-		 */
-		if (Array.isArray(header)) {
-			header.push("interest-cohort=()");
-			res.header("Permissions-Policy", header);
-			return;
-		}
+			/**
+			 * Headers can be returned as an array.
+			 * @see {@link https://nodejs.org/docs/latest/api/http.html#requestsetheadername-value | Node.js HTTP API}
+			 */
+			if (Array.isArray(header)) {
+				header.push("interest-cohort=()");
+				res.header("Permissions-Policy", header);
+				return;
+			}
 
-		if (header) {
-			res.header("Permissions-Policy", `${header}, interest-cohort=()`);
-		} else {
-			res.header("Permissions-Policy", "interest-cohort=()");
+			if (header) {
+				res.header(
+					"Permissions-Policy",
+					`${header}, interest-cohort=()`
+				);
+			} else {
+				res.header("Permissions-Policy", "interest-cohort=()");
+			}
 		}
-	});
+	);
 }
 
 module.exports = fp(fastifyFlocOff, {
