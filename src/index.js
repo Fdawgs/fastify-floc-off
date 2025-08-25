fb6eb2c8b3d083b5b00a4062722bc015859f6bad
Frazer Smith <frazer.dev@icloud.com>
2025-08-25 13:40:00 +0100

    perf(index): hoist `setFlocPermissionsHeader` to module scope (#289)

--- src/index.js
+++ src/index.js
@@ -6,33 +6,38 @@ const DIRECTIVE = "interest-cohort=()";
 
 /**
  * @author Frazer Smith
+ * @description Sets/adds the "interest-cohort=()" directive to the
+ * Permissions-Policy response header.
+ * @type {import("fastify").onRequestHookHandler}
+ */
+function setFlocPermissionsHeader(_req, res, next) {
+	const header = res.getHeader("Permissions-Policy");
+
+	if (Array.isArray(header)) {
+		if (!header.some((item) => item.includes(DIRECTIVE))) {
+			header.push(DIRECTIVE);
+			res.header("Permissions-Policy", header);
+		}
+	} else if (typeof header === "string") {
+		if (!header.includes(DIRECTIVE)) {
+			res.header("Permissions-Policy", `${header}, ${DIRECTIVE}`);
+		}
+	} else {
+		// No header exists yet
+		res.header("Permissions-Policy", DIRECTIVE);
+	}
+	next();
+}
+
+/**
+ * @author Frazer Smith
  * @description Simple plugin that adds an `onRequest` hook to opt out of Google's FLoC
  * advertising-surveillance network by setting/adding the "interest-cohort=()" directive
  * to the Permissions-Policy response header.
  * @type {import("fastify").FastifyPluginCallback}
  */
 function fastifyFlocOff(server, _options, done) {
-	server.addHook(
-		"onRequest",
-		function setFlocPermissionsHeader(_req, res, next) {
-			const header = res.getHeader("Permissions-Policy");
-
-			if (Array.isArray(header)) {
-				if (!header.some((item) => item.includes(DIRECTIVE))) {
-					header.push(DIRECTIVE);
-					res.header("Permissions-Policy", header);
-				}
-			} else if (typeof header === "string") {
-				if (!header.includes(DIRECTIVE)) {
-					res.header("Permissions-Policy", `${header}, ${DIRECTIVE}`);
-				}
-			} else {
-				// No header exists yet
-				res.header("Permissions-Policy", DIRECTIVE);
-			}
-			next();
-		}
-	);
+	server.addHook("onRequest", setFlocPermissionsHeader);
 	done();
 }
 
